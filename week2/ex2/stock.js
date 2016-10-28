4fd8e13cc3c4336479c6f8032dc63dcb879dec9a
xonatialee <xonatialee@dyn-160-39-121-131.secure.barnard.edu>
2016-10-27 20:26:34 -0400

    fixing merge errors

--- week2/ex2/stock.js
+++ week2/ex2/stock.js
@@ -1,1 +1,17 @@
+//The stock price starts at 10 dollars. 
+//While the stock price is greater than or equal to 10, 
+//confirm the new stock price. If the stock price falls 
+//below 10 dollars, alert the user that they should sell! 
+//Stop asking what the price is.
+
 console.log('working!');
+
+var stockprice = 10; 
+
+while(stockprice >= 10) {
+	alert(stockprice); 
+	alert(stockprice = stockprice - 1);
+    if(stockprice < 10){
+      alert("You should stop selling!")
+    }
+} 
